b3cd3b7c0d5933b3eff2d9a84da678befc7582de
Kellen Parker <kellenrparker@gmail.com>
2021-09-05 13:45:53 -0500

    implemeted grid, modules, and a simple clock

--- public/electron.js
+++ public/electron.js
@@ -19,7 +19,7 @@ function createWindow() {
   win.loadURL(
     isDev
       ? 'http://localhost:3000'
-      : `file://${path.join(__dirname, '../build/index.html')}`
+      : `file://${path.join(__dirname, 'index.html')}`
   );
   // Open the DevTools.
   if (isDev) {
@@ -27,14 +27,8 @@ function createWindow() {
   }
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
 app.whenReady().then(createWindow);
 
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
     app.quit();
