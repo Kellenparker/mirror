83e0885d9977fe7c380cdca1c546b2737fb1c64b
Kellen Parker <kellenrparker@gmail.com>
2021-09-07 10:38:27 -0500

    better implementation of database

---
+++ src/db.js
@@ -0,0 +1,24 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getDatabase, ref, onValue} from "firebase/database";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBVLFOHZH78eU1L3G0i4y7jHvnCQ3bBn0o",
+  authDomain: "mirror-c9884.firebaseapp.com",
+  databaseURL: "https://mirror-c9884-default-rtdb.firebaseio.com",
+  projectId: "mirror-c9884",
+  storageBucket: "mirror-c9884.appspot.com",
+  messagingSenderId: "34657405784",
+  appId: "1:34657405784:web:15e50fdaa189c02b34f32d",
+  measurementId: "G-DE67V3RQ71"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const db = getDatabase();
+const timeRef = ref(db, "time/");
+
+function writeData() {
+  console.log("hello");
+}
--- src/index.js
+++ src/index.js
@@ -1,5 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Clock from './modules/Clock/Clock.js';
+import { app } from './db.js'
+import { getDatabase, ref, onValue} from "firebase/database";
+
+const db = getDatabase();
+const timeRef = ref(db, "time/");
+
+onValue(timeRef, (snapshot) => {
+  const data = snapshot.val();
+  console.log(data);
+  console.log("hello");
+})
 
 ReactDOM.render(<Clock/>, document.getElementById('mid1'));
