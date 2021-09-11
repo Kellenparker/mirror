2fdf9d30b42ecde705e6db2ceaa0ee76ab410bcb
Kellen Parker <kellenrparker@gmail.com>
2021-09-11 12:12:34 -0500

    added extension to day

--- src/modules/Clock/Clock.js
+++ src/modules/Clock/Clock.js
@@ -12,6 +12,7 @@ class Clock extends React.Component {
       year: this.currentTime.getFullYear(),
     };
     this.monthName = this.monthName.bind(this);
+    this.extension = this.extension.bind(this);
   }
   componentDidMount() {
     this.interval = setInterval(
@@ -37,6 +38,17 @@ class Clock extends React.Component {
     "July", "August", "September", "October", "November", "December"]
     return monthNames[month];
   }
+  extension(day){
+    if((day >= 4 && day < 21) || (day >= 24 && day < 31)){
+      return 'th';
+    }else if(day === 1 || day === 21 || day === 31){
+      return 'st';
+    }else if(day === 2 || day === 22){
+      return 'nd';
+    }else if(day === 3 || day === 23){
+      return 'rd';
+    }
+  }
   render() {
     return (
       <div style={{
@@ -49,11 +61,11 @@ class Clock extends React.Component {
       }}>
         <h3 className="date" style={{fontSize: "2vh",
         opacity: this.props.disabled ? '0' : '1'}}>
-          {this.monthName(this.state.month)} {this.state.day}, {this.state.year}
+          {this.monthName(this.state.month)} {this.state.day}{this.extension(this.state.day)}, {this.state.year}
         </h3>
         <h3 className="time" style={{fontSize: "3vh",
         opacity: this.props.disabled ? '0' : '1'}}>
-          {this.state.hour % 12}:{this.state.minute < 10 ? ("0" + this.state.minute) : this.state.minute} {this.state.hour >= 12 ? "PM" : "AM"}
+          {this.state.hour > 12 ? this.state.hour % 12 : this.state.hour}:{this.state.minute < 10 ? ("0" + this.state.minute) : this.state.minute} {this.state.hour >= 12 ? "PM" : "AM"}
         </h3>
       </div>
     );
