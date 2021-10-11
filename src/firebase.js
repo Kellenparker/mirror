816c239801c35119005a0350eb0a1b558a970f5b
Kellen Parker <kellenrparker@gmail.com>
2021-10-11 16:57:35 -0500

    added google vision api

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,4 @@
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBVLFOHZH78eU1L3G0i4y7jHvnCQ3bBn0o",
@@ -15,5 +13,3 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-const db = getDatabase();
--- src/server.js
+++ src/server.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const PiCamera = require('pi-camera');
+const {spawn} = require('child_process');
+const {Storage} = require('@google-cloud/storage');
 const app = express()
 const cors = require('cors')
 const port = 3001
@@ -9,6 +11,21 @@ app.use(cors())
 //You can use this to check if your server is working
 app.get('/', (req, res)=>{
     res.send("Welcome to your server")
+	const storage = new Storage({projectId: 'genial-core-326621', keyFilename: 'genial-core-326621-00c4218e4202.json'});
+	// Makes an authenticated API request.
+	async function listBuckets() {
+		try {
+		const [buckets] = await storage.getBuckets();
+	
+		console.log('Buckets:');
+		buckets.forEach(bucket => {
+			console.log(bucket.name);
+		});
+		} catch (err) {
+		console.error('ERROR:', err);
+		}
+	}
+	listBuckets();
 })
 
 //Start your server on a specified port
@@ -20,9 +37,9 @@ app.get('/capture', function(req, res) {
 	console.log('capture');
 	const myCamera = new PiCamera({
 		mode: 'photo',
-		output: `${ __dirname }/test.jpg`,
-		width: 640,
-		height: 480,
+		output: `${ __dirname }/capture/img.jpg`,
+		width: 1280,
+		height: 900,
 		nopreview: true,
 	});
 	myCamera.snap()
@@ -32,4 +49,19 @@ app.get('/capture', function(req, res) {
 		.catch((error) => {
 			console.log("error");
 		});
+	
+	async function printLabels() {
+		// Imports the Google Cloud client library
+		const vision = require('@google-cloud/vision');
+	  
+		// Creates a client
+		const client = new vision.ImageAnnotatorClient();
+	  
+		// Performs label detection on the image file
+		const [result] = await client.labelDetection(`${ __dirname }/capture/img.jpg`);
+		const labels = result.labelAnnotations;
+		console.log('Labels:');
+		labels.forEach(label => console.log(label.description));
+	  }
+	  printLabels();
 });
