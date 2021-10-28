d506c5e4646d578e37d7cbb0f8248175d44c37d4
Kellen Parker <kellenparker@Elizabeths-MacBook-Pro.local>
2021-10-28 08:28:49 -0500

    clothing labels are filtered

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ const PiCamera = require('pi-camera');
 const {spawn} = require('child_process');
 const {Storage} = require('@google-cloud/storage');
 const imageToBase64 = require('image-to-base64');
+const {searchAmazon, AmazonSearchResult} = require('unofficial-amazon-search');
 const app = express()
 const cors = require('cors')
 const port = 3001
@@ -77,13 +78,30 @@ app.get('/capture', function(req, res) {
 			]
 		};
 		// Performs label detection on the image file
-		client.annotateImage(request)
+		const apprLabels = ['Apron', 'Coat', 'Collar', 'Costume', 'Dress', 'Hoodie', 'Jacket', 'Jersey', 'Shirt', 'Blouse',
+							'Sweater', 'Sweatshirt', 'Vest', 'T-shirt'];
+
+		
+		
+		const clothingLabels = [];
+
+		let res = client.annotateImage(request)
 			.then((result) => {
 				console.log(result[0].labelAnnotations);
 				let labels = result[0].labelAnnotations;
 				console.log('Labels:');
 				labels.forEach(label => label.score > .5 ? console.log(label.description) : 0);
+				labels.forEach(function(label){
+					for(let i = 0; i < apprLabels.length; i++){
+						if(label.description === apprLabels[i]){
+							console.log('here');
+							clothingLabels.push(label.description);
+						}
+					}
+				});
+				console.log(clothingLabels);
 			});
+		
 		
 	  }
 	  buildLink();
