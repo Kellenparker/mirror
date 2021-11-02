f4536b6135cadea8a4cc566030e84ec9b0960ec5
Kellen Parker <kellenrparker@gmail.com>
2021-11-02 14:00:14 -0500

    changed location values to unique number ids

--- src/index.js
+++ src/index.js
@@ -2,47 +2,47 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Clock from './modules/Clock/Clock.js';
 import { app } from './firebase.js'
-import { getDatabase, ref, onValue, set} from "firebase/database";
+import { getDatabase, ref, onValue, set } from "firebase/database";
 var request = require('request');
 
 // Call localhost:3001/ to initialize Firebase
 var clientServerOptions = {
-  uri: 'http://localhost:3001/',
+	uri: 'http://localhost:3001/',
 }
 request(clientServerOptions, function (error, response) {
-    console.log(error,response.body);
-    return;
+	console.log(error, response.body);
+	return;
 });
 
 const db = getDatabase();
 const timeRef = ref(db, "time/");
 var timeData;
 onValue(timeRef, (snapshot) => {
-  timeData = snapshot.val();
-  console.log(timeData);
-  ReactDOM.render(<Clock disabled={timeData}/>, document.getElementById('mid1'));
+	timeData = snapshot.val();
+	console.log(timeData);
+	ReactDOM.render(<Clock disabled={timeData} />, document.getElementById('loc4'));
 })
 
 // Get capture value
-const captureRef = ref(db, "camera/capture/");
-const cameraRef = ref(db, "camera/");
+const captureRef = ref(db, "scan/camera/capture/");
+const cameraRef = ref(db, "scan/camera/");
 var captureData;
 onValue(captureRef, (snapshot) => {
-  captureData = snapshot.val();
-  console.log(captureData);
+	captureData = snapshot.val();
+	console.log(captureData);
 
-  // If capture is true, initialize server side scan function
-  if(captureData){
-    var clientServerOptions = {
-      uri: 'http://localhost:3001/capture',
-    }
-    request(clientServerOptions, function (error, response) {
-        console.log(error,response.body);
-        return;
-    });
-    // Set capture back to false after calling server function
-    set(cameraRef, {
-      capture: false
-    });
-  }
+	// If capture is true, initialize server side scan function
+	if (captureData) {
+		var clientServerOptions = {
+			uri: 'http://localhost:3001/capture',
+		}
+		request(clientServerOptions, function (error, response) {
+			console.log(error, response.body);
+			return;
+		});
+		// Set capture back to false after calling server function
+		set(cameraRef, {
+			capture: false
+		});
+	}
 })
--- src/server.js
+++ src/server.js
@@ -1,11 +1,11 @@
 const express = require('express')
 const PiCamera = require('pi-camera');
-const {spawn} = require('child_process');
-const {Storage} = require('@google-cloud/storage');
+const { spawn } = require('child_process');
+const { Storage } = require('@google-cloud/storage');
 const { initializeApp } = require('firebase/app');
-const { getDatabase, ref, onValue, set, update, removeValue} = require('firebase/database');
+const { getDatabase, ref, onValue, set, update, removeValue } = require('firebase/database');
 const imageToBase64 = require('image-to-base64');
-const {searchAmazon, AmazonSearchResult} = require('unofficial-amazon-search');
+const { searchAmazon, AmazonSearchResult } = require('unofficial-amazon-search');
 const app = express()
 const cors = require('cors')
 const port = 3001
@@ -13,8 +13,8 @@ const port = 3001
 app.use(cors())
 
 //You can use this to check if your server is working
-app.get('/', (req, res)=>{
-    res.send('Welcome to your server')
+app.get('/', (req, res) => {
+	res.send('Welcome to your server')
 
 	// Configure Firebase API
 	const firebaseConfig = {
@@ -26,21 +26,21 @@ app.get('/', (req, res)=>{
 		messagingSenderId: '34657405784',
 		appId: '1:34657405784:web:15e50fdaa189c02b34f32d',
 		measurementId: 'G-DE67V3RQ71'
-	  };
-	  
+	};
+
 	// Initialize Firebase
 	const app = initializeApp(firebaseConfig);
 })
 
-//Start your server on a specified port
-app.listen(port, ()=>{
-    console.log(`Server is running on port ${port}`)
+// Start your server on a specified port
+app.listen(port, () => {
+	console.log(`Server is running on port ${port}`)
 })
 
-app.get('/remove', function(req, res){
+app.get('/remove', function (req, res) {
 	// Load database
 	const db = getDatabase();
-	const linksRef = ref(db, 'links');
+	const linksRef = ref(db, 'scan/links');
 
 	// Remove all links by using set function
 	set(linksRef, {
@@ -48,13 +48,13 @@ app.get('/remove', function(req, res){
 	});
 })
 
-app.get('/capture', function(req, res) {
+app.get('/capture', function (req, res) {
 	console.log('capture');
-	
+
 	// Captures an image using raspberry pi camera
 	const myCamera = new PiCamera({
 		mode: 'photo',
-		output: `${ __dirname }/capture/img.jpg`,
+		output: `${__dirname}/capture/img.jpg`,
 		width: 1280,
 		height: 900,
 		nopreview: true,
@@ -66,23 +66,23 @@ app.get('/capture', function(req, res) {
 		.catch((error) => {
 			console.log('error');
 		});
-	
+
 	async function buildLink() {
 		// Imports the Google Cloud client library
 		const vision = require('@google-cloud/vision');
-	  
+
 		// Creates an client for use in Google Cloud API
 		const client = new vision.ImageAnnotatorClient();
 
 		// Variable to hold base64 version of image
-		var img
+		var img;
 
 		// Convert captured image to base64 for use in annotateImage request
-		await imageToBase64(`${ __dirname }/capture/img.jpeg`)
+		await imageToBase64(`${__dirname}/capture/img2.jpg`)
 			.then((response) => {
-				img = response
+				img = response;
 			})
-		
+
 		// Create request for annotateImage
 		const request = {
 			image: {
@@ -96,11 +96,11 @@ app.get('/capture', function(req, res) {
 				}
 			]
 		};
-		
+
 		// All appropriate clothing labels
 		const apprLabels = ['Apron', 'Bodybuilding', 'Coat', 'Costume', 'Dress', 'Hoodie', 'Jacket', 'Jersey', 'Shirt', 'Blouse', 'Sportswear',
-							'Sweater', 'Sweatshirt', 'Vest', 'T-shirt', 'Suit', 'Blazer', 'Dress shirt', 'Formal wear'];
-		
+			'Sweater', 'Sweatshirt', 'Vest', 'T-shirt', 'Suit', 'Blazer', 'Dress shirt', 'Formal wear'];
+
 		// Array that will hold only the clothing labels
 		const clothingLabels = [];
 
@@ -110,23 +110,23 @@ app.get('/capture', function(req, res) {
 				let labels = result[0].labelAnnotations;
 				console.log('Labels:');
 				labels.forEach(label => label.score > .5 ? console.log(label.description) : 0);
-				
+
 				// Sort through all returned labels to find appropriate labels, determined by apprLabels
-				labels.forEach(function(label){
-					const len = apprLabels.length
-					for(let i = 0; i < len; i++){
-						if(label.description === apprLabels[i]){
+				labels.forEach(function (label) {
+					const len = apprLabels.length;
+					for (let i = 0; i < len; i++) {
+						if (label.description === apprLabels[i]) {
 							// Update clothingLabels array with filtered labels
 							clothingLabels.push(label.description);
 						}
 					}
 				});
-				
+
 				console.log(clothingLabels);
 
 				// Build search string 
-				let searchStr = 'clothes ';
-				for(let i = 0; i < clothingLabels.length; i++)
+				let searchStr = '';
+				for (let i = 0; i < clothingLabels.length; i++)
 					searchStr += clothingLabels[i] + ' ';
 
 				// Load database
@@ -136,22 +136,22 @@ app.get('/capture', function(req, res) {
 				const userRef = ref(db, 'user');
 				var ageData, genderData;
 				var ageStr = genderStr = '';
+
 				// Get values of age and gender, will only run once
 				onValue(userRef, (snapshot) => {
 					ageData = snapshot.child('age').val();
 					genderData = snapshot.child('gender').val();
 
-					if(ageData < 12)
+					if (ageData < 12)
 						ageStr = 'child';
-					else if(ageData < 18)
+					else if (ageData < 18)
 						ageStr = 'teen';
 					else
 						ageStr = 'adult';
 
-					
-					if(genderData === 0)
+					if (genderData === 0)
 						genderStr = 'male';
-					else
+					else if (genderData === 1)
 						genderStr = 'female';
 
 					// Add gender and age to search string
@@ -159,9 +159,9 @@ app.get('/capture', function(req, res) {
 					console.log(searchStr);
 
 					// Grab amazon search results using search strings
-					let results = searchAmazon(searchStr, {maxResults: 20}).then(data => {
-						
-						const linksRef = ref(db, 'links');
+					let results = searchAmazon(searchStr).then(data => {
+
+						const linksRef = ref(db, 'scan/links');
 
 						console.log(data);
 
@@ -175,23 +175,23 @@ app.get('/capture', function(req, res) {
 							const currentLink = 'link' + i
 							update(linksRef, {
 								[currentLink]: true,
-							})
+							});
 
 							// Set link child with link values
-							const curLinkRef = ref(db, 'links/' + currentLink);
+							const curLinkRef = ref(db, 'scan/links/' + currentLink);
 							set(curLinkRef, {
 								linkTitle: result.title,
 								linkUrl: result.productUrl,
 								linkImg: result.imageUrl
-							})
+							});
 							count = i;
 
 						});
 
 						// Update link count in database
 						update(linksRef, {
-							linkAmt: {count}
-						})
+							linkAmt: { count }
+						});
 
 
 					});
@@ -199,10 +199,10 @@ app.get('/capture', function(req, res) {
 				}, {
 					// Makes the onValue function only run once
 					onlyOnce: true
-				});	
+				});
 
 			});
-		
-	  }
-	  buildLink();
+
+	}
+	buildLink();
 });
